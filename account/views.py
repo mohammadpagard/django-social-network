9510b903d0fe747318b181cf9877ec3cf723b5cb
mohammadpagard <mohammadpagard2003@gmail.com>
2022-07-24 12:38:03 +0430

    Validation register fields & Login & Logout

--- account/views.py
+++ account/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegisterationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 
-class RegisterView(View):
-    form_class = UserRegisterationForm
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
     template_name = 'account/register.html'
 
     def get(self, request):
@@ -17,8 +18,36 @@ class RegisterView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
             messages.success(request, 'You registered successfully', 'success')
             return redirect('home:home')
 
         return render(request, self.template_name, {'form': form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self, requset):
+        form = self.form_class()
+        return render(requset, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You logged in successfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'Your username or password is wrong', 'warning')
+        return render(request, self.template_name, {'form': form})
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'You logout successfully', 'success')
+        return redirect('home:home')
