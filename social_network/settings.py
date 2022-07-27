68349668155c4a22f0b7ba1ef158bfeeb30847c9
mohammadpagard <mohammadpagard2003@gmail.com>
2022-07-27 13:29:10 +0430

    Reset password forgot & validation in userprofile for user.firstname and user.lastname.

--- social_network/settings.py
+++ social_network/settings.py
@@ -132,3 +132,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = '/account/login/'
+
+# Google account
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'pagardmohammad01@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'ldymfbmtbdbhjsom'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Social network'
